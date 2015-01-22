71c86cd40f1deae97f591c703ea470f54c27a8d2
Vincent Fleischacker <vdynmx@gmail.com>
2015-01-22 08:06:00 +0000

    Added CORS in eventscontroller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  
+  before_filter :set_headers
+
   def index
     @events = Event.all
   end
@@ -18,5 +21,15 @@ class EventsController < ApplicationController
    # @event = Event.new(params.require(:event).permit(:name, :property_1, :property_2))
    # @event.save
   #end
+
+  private
+
+  def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+      headers['Access-Control-Max-Age'] = '1728000'
+  end
 
 end
