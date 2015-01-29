258663c6da837acdab9c2dc3d6f9e30a3623acbd
Vincent Fleischacker <vdynmx@gmail.com>
2015-01-29 05:41:51 +0000

    rewriting js snippet

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -53,5 +53,12 @@ class Api::V1::EventsController
     error(403, 'Permission Denied!')
   end
 
+  def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+      headers['Access-Control-Max-Age'] = '1728000'
+  end
 
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,15 +21,4 @@ class EventsController < ApplicationController
    # @event = Event.new(params.require(:event).permit(:name, :property_1, :property_2))
    # @event.save
   #end
-
-  private
-
-  def set_headers
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Expose-Headers'] = 'ETag'
-      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-      headers['Access-Control-Max-Age'] = '1728000'
-  end
-
 end
