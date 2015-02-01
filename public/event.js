9f95d0e32f4cd7514d2ff20cd18336fcd2632402
Vincent Fleischacker <vdynmx@gmail.com>
2015-02-01 14:58:51 +0000

    json append

--- public/event.js
+++ public/event.js
@@ -5,7 +5,7 @@ var trackEvent = function(name, eventtime) {
   }
 
   var _bm_request = $.ajax({
-    url: "https://secure-earth-4066.herokuapp.com/events",
+    url: "https://secure-earth-4066.herokuapp.com/events.json",
     method: "post",
     data: {event: _bm_event}
   })
